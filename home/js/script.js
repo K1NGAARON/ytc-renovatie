671fc533234734937fe5da2d0e29588d8ccbed6e
K1NGAARON <gerardaaron1997@gmail.com>
2023-10-03 14:58:23 +0200

    changes

--- home/js/script.js
+++ home/js/script.js
@@ -1,16 +1,16 @@
 function changeReviews() {
     const reviews = [
         {
-            name: 'Kim',
-            review: 'Ik ben super tevreden over hoe Thomas mijn 2 bovenverdiepingen heeft gerenoveerd. Hij denkt mee over mogelijke oplossingen, hij levert kwaliteit en de communicatie liep altijd vlot. Sowieso zal hij de rest van mijn huis in de toekomst mogen aanpakken!!',
+            name: 'Klant 1',
+            review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, illum magnam. Adipisci obcaecati hic quaerat, molestias consequatur, ratione tempora tenetur officia neque suscipit earum iusto, consequuntur rem. Dolorem, cum!',
         },
         {
-            name: 'Gery',
-            review: 'Samenwerken met ARPV was super! De firma heeft onze zolder geïsoleerd en afgewerkt tot een mooie relaxruimte. Tevens heeft hij onze verwarming nagezien en verbeterd en de regenwatervoorziening aangepast. Er was hierbij steeds een vlotte communicatie en een duidelijke en correcte prijsofferte en prijssetting. De afgesproken timing werd gevolgd. Ook de afwerking is prima. Thomas gaf goede suggesties, steeds met aandacht voor kwaliteit én prijsbewust. Hij was heel betrokken bij ons hele project. En op en top vriendelijk!',
+            name: 'Klant 2',
+            review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, illum magnam. Adipisci obcaecati hic quaerat, molestias consequatur, ratione tempora tenetur officia neque suscipit earum iusto, consequuntur rem. Dolorem, cum!',
         },
         {
-            name: 'Yoshi',
-            review: 'Wij werkten samen met ARPV voor een totaalrenovatie van onze woning. Vanaf de ruwbouw tot de afwerking van de ruimtes werden alle werken met veel verantwoordelijkheid en oog voor detail uitgevoerd. Naast de werken zelf konden we ook steeds terecht bij ARPV voor vragen of goede ideeën omtrent afwerking en opbouw. Kortom een aannemer die mee helpt werken en denken aan het eindresultaat. 1000x bedankt voor wat je ons gezin gegeven hebt :) Geen vaarwel, maar een tot ziens!',
+            name: 'Klant 3',
+            review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, illum magnam. Adipisci obcaecati hic quaerat, molestias consequatur, ratione tempora tenetur officia neque suscipit earum iusto, consequuntur rem. Dolorem, cum!',
         }
     ];
 
